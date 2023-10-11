b418a5b5bd826deb43e276620f4c753bd1bd31e0
uttamsharmaincedoinc <141917241+uttamsharmaincedoinc@users.noreply.github.com>
2023-10-12 01:05:19 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -9,8 +9,8 @@ using namespace std;
 #define nl "\n"
 #define pb push_back
 #define printvec(arr) for(auto &x:(arr))cout<<x<<" "
-#define lbnm lower_bound
-#define ubnm upper_bound
+#define lbnd lower_bound
+#define ubnd upper_bound
 
 
 //-------------------------------------------------------------
